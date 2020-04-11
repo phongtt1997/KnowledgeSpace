5b2979584115647a5af2fdf0d956eb7123464214
phongtt1997 <ttphong2019@gmail.com>
2020-04-11 23:10:06 +0700

    Command manage API

---
+++ src/BackendServer/Controllers/CommandsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendServer.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViewModels.Systems;
+
+namespace BackendServer.Controllers
+{
+    public class CommandsController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+        public CommandsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet()]
+        public async Task<IActionResult> GetCommants()
+        {
+            var commands = _context.Commands;
+            var commandVms = await commands.Select(x => new CommandVm()
+            {
+                Id = x.Id,
+                Name = x.Name,
+            }).ToListAsync();
+            return Ok(commandVms);
+        }
+    }
+}
